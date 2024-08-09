8e488e3725714d68b414a3beaf407b099d6a0bc9
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-08-09 19:53:10 +0200

    created theme.js

---
+++ src/common/config/theme.js
@@ -0,0 +1,45 @@
+export const theme = {
+    color: {
+        white: "#FFFFFF",
+        whisper: "#F5F5FA",
+        mystic: "#E4E6F0",
+        waterloo: "#7E839A",
+        stormGrey: "#74788B",
+        woodsmoke: "#18181B",
+        mineShaft: "#333333",
+        heater: "#BAC7D5",
+        pattensBlue: "#D6E4FF",
+        scienceBlue: "#0044CC",
+        black: "#000000",
+    },
+    breakpoint: {
+        laptop: "1480px",
+        tablet: "1278px",
+        tabletOne: "1120px",
+        tabletTwo: "1045px",
+        tabletPortrait: "990px",
+        phone: "767px",
+        phoneTwo: "700px",
+        phonePortrait: "481px"
+    },
+    fontWeight: {
+        regular: "400",
+        medium: "500",
+        semiBold: "600",
+        bold: "700",
+    },
+    fontSize: {
+        xs: "10px",
+        sm: "13px",
+        md: "14px",
+        lg: "16px",
+        xl: "18px",
+        xl2: "20px",
+        xl3: "22px",
+        xl4: "24px",
+        xl5: "28px",
+        xl6: "36px",
+        xl7: "48px",
+        xl8: "64px",
+    }
+};
