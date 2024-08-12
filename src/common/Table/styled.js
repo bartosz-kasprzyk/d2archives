e3006a4afb244a4c661279d75e6401ece77b7662
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-08-12 23:51:37 +0200

    fixed the colors of the table

--- src/common/Table/styled.js
+++ src/common/Table/styled.js
@@ -13,21 +13,32 @@ export const TableCell = styled.th`
     border: 3px solid #000;
     padding: 10px;
     text-align: center;
+    color: #dddddd;
 `;
 
-export const ColumnHeader = styled(TableCell)`
+export const ColumnHeader = styled.th`
+    border: 3px solid #000;
+    padding: 10px;
+    text-align: center;
     color: #df9c0c;
     background: #0c0c0c;
 `;
 
-export const RowHeader = styled(TableCell)`
+export const RowHeader = styled.th`
+    border: 3px solid #000;
+    padding: 10px;
+    text-align: center;
     color: #df9c0c;
 `;
 
 export const TableRow = styled.tr`
     background-color: ${({ index }) => (index % 2 === 0 ? '#141313' : '#201f1f')};
     
-    :hover {
+    &:hover {
         background-color: #3b2d1c;
     }
+`;
+
+export const StyledRune = styled.img`
+    width: 30px;
 `;
