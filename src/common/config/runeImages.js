838fea6d512360014ad8b7da334925e6004a462f
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-08-12 23:50:09 +0200

    added all rune images

---
+++ src/common/config/runeImages.js
@@ -0,0 +1,71 @@
+import RuneEl from '../../images/RuneEl.png';
+import RuneEld from '../../images/RuneEld.png';
+import RuneTir from '../../images/RuneTir.png';
+import RuneNef from '../../images/RuneNef.png';
+import RuneEth from '../../images/RuneEth.png';
+import RuneIth from '../../images/RuneIth.png';
+import RuneTal from '../../images/RuneTal.png';
+import RuneRal from '../../images/RuneRal.png';
+import RuneOrt from '../../images/RuneOrt.png';
+import RuneThul from '../../images/RuneThul.png';
+import RuneAmn from '../../images/RuneAmn.png';
+import RuneSol from '../../images/RuneSol.png';
+import RuneShael from '../../images/RuneShael.png';
+import RuneDol from '../../images/RuneDol.png';
+import RuneHel from '../../images/RuneHel.png';
+import RuneIo from '../../images/RuneIo.png';
+import RuneLum from '../../images/RuneLum.png';
+import RuneKo from '../../images/RuneKo.png';
+import RuneFal from '../../images/RuneFal.png';
+import RuneLem from '../../images/RuneLem.png';
+import RunePul from '../../images/RunePul.png';
+import RuneUm from '../../images/RuneUm.png';
+import RuneMal from '../../images/RuneMal.png';
+import RuneIst from '../../images/RuneIst.png';
+import RuneGul from '../../images/RuneGul.png';
+import RuneVex from '../../images/RuneVex.png';
+import RuneOhm from '../../images/RuneOhm.png';
+import RuneLo from '../../images/RuneLo.png';
+import RuneSur from '../../images/RuneSur.png';
+import RuneBer from '../../images/RuneBer.png';
+import RuneJah from '../../images/RuneJah.png';
+import RuneCham from '../../images/RuneCham.png';
+import RuneZod from '../../images/RuneZod.png';
+
+const runeImages = {
+    El: RuneEl,
+    Eld: RuneEld,
+    Tir: RuneTir,
+    Nef: RuneNef,
+    Eth: RuneEth,
+    Ith: RuneIth,
+    Tal: RuneTal,
+    Ral: RuneRal,
+    Ort: RuneOrt,
+    Thul: RuneThul,
+    Amn: RuneAmn,
+    Sol: RuneSol,
+    Shael: RuneShael,
+    Dol: RuneDol,
+    Hel: RuneHel,
+    Io: RuneIo,
+    Lum: RuneLum,
+    Ko: RuneKo,
+    Fal: RuneFal,
+    Lem: RuneLem,
+    Pul: RunePul,
+    Um: RuneUm,
+    Mal: RuneMal,
+    Ist: RuneIst,
+    Gul: RuneGul,
+    Vex: RuneVex,
+    Ohm: RuneOhm,
+    Lo: RuneLo,
+    Sur: RuneSur,
+    Ber: RuneBer,
+    Jah: RuneJah,
+    Cham: RuneCham,
+    Zod: RuneZod,
+};
+
+export default runeImages;
