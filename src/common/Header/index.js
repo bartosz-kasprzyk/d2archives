024ad5007b5d2cbcdb8f3954d28aa7ca209f5573
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-08-22 00:50:05 +0200

    moved LanguageSwitch to Header instead of Navigation

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -1,8 +1,10 @@
 import { StyledHeader } from "./styled";
 import { Navigation } from "./Navigation";
+import LanguageSwitch from "./LanguageSwitch";
 
 export const Header = () => (
     <StyledHeader>
+        <LanguageSwitch />
         <Navigation />
     </StyledHeader>
 );
