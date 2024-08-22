151ecc0e92140deb25e502be0c1fac5d18264f6c
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-08-22 20:35:31 +0200

    added a gradient to the background

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -14,7 +14,9 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     body {
-        background: ${({ theme }) => theme.color.black};
+        margin: auto;
+        background: linear-gradient(90deg, rgba(0, 0, 0, 1) 0%, rgba(31, 18, 0, 1) 55%, rgba(0, 0, 0, 1) 100%);
+        color: ${({ theme }) => theme.color.white};
         font-family: 'Inter', sans-serif;
     }
 `;
