a3f07c478111328e802d0ba87fcb143cf4e45d9a
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-08-30 17:39:18 +0200

    Increased the size of RuneButtons and added scale transition on hover

--- src/features/RuneList/styled.js
+++ src/features/RuneList/styled.js
@@ -8,27 +8,32 @@ export const StyledText = styled.p`
 export const RuneGridContainer = styled.div`
     display: grid;
     justify-content: center;
-    grid-template-columns: repeat(11, 45px);
+    grid-template-columns: repeat(11, 60px);
     grid-template-rows: repeat(3, auto);
-    gap: 10px;
+    gap: 15px;
     margin: 20px 0;
 `;
 
 export const RuneButton = styled.button`
     background: none;
     border: none;
-    padding: 0;
+    padding: 5px;
     margin: 0;
-    width: 45px;
-    height: 45px;
+    width: 60px;
+    height: 60px;
     display: flex;
     align-items: center;
     justify-content: center;
     cursor: pointer;
     overflow: hidden;
+    transition: transform ease 300ms;
     
     &:focus {
         outline: none;
+    }
+
+    &:hover {
+        transform: scale(1.20);
     }
 `;
 
