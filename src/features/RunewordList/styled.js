a03b0206473c68e3814a7ec29fe467dd5696d1e1
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-09-02 23:49:01 +0200

    added styled ValueBox for range of values

--- src/features/RunewordList/styled.js
+++ src/features/RunewordList/styled.js
@@ -1,4 +1,13 @@
 import styled from "styled-components"
+import TopLeft from "../../images/ui/tt_tl_tiny_i_m_bb.webp"
+import TopRight from "../../images/ui/tt_tr_tiny_i_m_bb.webp"
+import BottomLeft from "../../images/ui/tt_bl_tiny_i_m_bb.webp"
+import BottomRight from "../../images/ui/tt_br_tiny_i_m_bb.webp"
+import TopLine from "../../images/ui/tt_border_t_tiny_i.webp"
+import BottomLine from "../../images/ui/tt_border_b_tiny_i.webp"
+import LeftLine from "../../images/ui/tt_border_l_tiny_i.webp"
+import RightLine from "../../images/ui/tt_border_r_tiny_i.webp"
+import Backdrop from "../../images/ui/bgtile_b.webp"
 
 export const StyledText = styled.p`
     color: ${({ theme }) => theme.color.white};
@@ -31,3 +40,17 @@ export const RunewordButton = styled.button`
         outline: none;
     }
 `
+
+export const ValueBox = styled.span`
+    padding: 2px;
+    background: 
+        url(${TopLeft}) top left no-repeat,
+        url(${TopRight}) top right no-repeat,
+        url(${BottomLeft}) bottom left no-repeat,
+        url(${BottomRight}) bottom right no-repeat,
+        url(${TopLine}) top repeat-x,
+        url(${BottomLine}) bottom repeat-x,
+        url(${LeftLine}) left repeat-y,
+        url(${RightLine}) right repeat-y,
+        url(${Backdrop}) center center repeat;
+`;
