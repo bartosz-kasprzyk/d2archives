48e0702b5252b39f6f677f5abf659d97d6578fef
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-09-06 03:58:10 +0200

    added a scroll to top on refresh

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import { Header } from './common/Header';
 import RuneList from './features/RuneList';
 import RunewordList from './features/RunewordList';
-import { HashRouter, Route, Navigate, Routes } from 'react-router-dom';
+import { HashRouter, Route, Navigate, Routes, useLocation } from 'react-router-dom';
 import { toRunes, toRunewords } from './common/config/routes';
 import { ScrollToTopButton } from './common/ScrollToTopButton';
+import useLoadContent from './common/hooks/useLoadContent';
+import { useEffect } from 'react';
+
+function ScrollToTop() {
+  const state = useLoadContent();
+  const status = state.status;
+
+  useEffect(() => {
+    if (status === 'loading') {
+      window.scrollTo(0, 0);
+    }
+  }, [status]);
+
+  return null;
+}
 
 function App() {
   return (
     <HashRouter>
       <Header />
+      <ScrollToTop />
 
       <Routes>
         <Route path={toRunes()} element={<RuneList />} />
