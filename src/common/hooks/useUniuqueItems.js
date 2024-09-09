9214237e5bc331f9c9d6056ee5bd4d9477236679
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-09-09 19:40:35 +0200

    deleted unnecessary lines

--- src/common/hooks/useUniuqueItems.js
+++ src/common/hooks/useUniuqueItems.js
@@ -3,19 +3,15 @@ import { useState, useEffect } from 'react';
 const useUniqueItems = () => {
     const [items, setItems] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
 
     useEffect(() => {
         const fetchItems = async () => {
             try {
                 const response = await fetch(`https://api.allorigins.win/raw?url=https://hellforge.vercel.app/api/v2/items/uniques`);
-                if (!response.ok) {
-                    throw new Error('Failed to fetch items');
-                }
                 const data = await response.json();
                 setItems(data);
-            } catch (err) {
-                setError(err.message);
+            } catch (error) {
+                console.log(error.message);
             } finally {
                 setLoading(false);
             }
@@ -24,7 +20,7 @@ const useUniqueItems = () => {
         fetchItems();
     }, []);
 
-    return { items, loading, error };
+    return { items, loading };
 };
 
 export default useUniqueItems;
