97925ff3500bb5060d43b41248b7a407dcefdab7
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-09-14 02:49:35 +0200

    deleted Navigation folder and moved the content straight to Header

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -1,10 +1,39 @@
-import { StyledHeader } from "./styled";
-import { Navigation } from "./Navigation";
-import LanguageSwitch from "./LanguageSwitch";
+import { toRunes, toRunewords, toUniques } from "../config/routes";
+import { LogoIcon1, LogoIcon2, LogoWrapper, NavSection, StyledCorner, StyledHeader, StyledNavLink, StyledUnderline } from "./styled";
+import d2r from "../../images/Logo/d2r.gif"
+import archives from "../../images/Logo/archives.png"
+import runes from "../../images/Navigation/RUNES.png"
+import runewords from "../../images/Navigation/RUNEWORDS.png"
+import uniques from "../../images/Navigation/UNIQUES.png"
+import underline from '../../images/underline.png'
 
-export const Header = () => (
-    <StyledHeader>
-        <LanguageSwitch />
-        <Navigation />
-    </StyledHeader>
-);
+export const Header = () => {
+    return (
+        <StyledHeader>
+            <StyledCorner $position="left" />
+            <StyledCorner />
+            <LogoWrapper to={toRunes()}>
+                <LogoIcon1 src={d2r} alt="Diablo II Logo" />
+                <LogoIcon2 src={archives} alt="Archives Logo" />
+            </LogoWrapper>
+            <NavSection>
+                <li>
+                    <StyledNavLink to={toRunes()}>
+                        <img src={runes} alt="Runes" />
+                    </StyledNavLink>
+                </li>
+                <li>
+                    <StyledNavLink to={toRunewords()}>
+                        <img src={runewords} alt="Runewords" />
+                    </StyledNavLink>
+                </li>
+                <li>
+                    <StyledNavLink to={toUniques()}>
+                        <img src={uniques} alt="Uniques" />
+                    </StyledNavLink>
+                </li>
+            </NavSection >
+            <StyledUnderline src={underline} />
+        </StyledHeader>
+    )
+};
--- src/common/Header/styled.js
+++ src/common/Header/styled.js
@@ -1,4 +1,8 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import flameImage from '../../images/flame.gif'
+import cornerImage from '../../images/corner.png'
 
 export const StyledHeader = styled.header`
     width: 100%;
@@ -7,3 +11,73 @@ export const StyledHeader = styled.header`
     align-items: center;
     justify-content: center;
 `
+
+export const LogoWrapper = styled(Link)`
+    display: flex;
+    flex-direction: column;
+    align-items: center;   
+    gap: 20px;         
+    grid-column: 1 / 3;
+    margin: 20px;
+`;
+
+export const LogoIcon1 = styled.img`
+    width: 300px;
+`;
+
+export const LogoIcon2 = styled.img`
+    width: 250px;
+    padding: 20px;
+    margin-top: -40px;
+    background-image: url(${flameImage});
+        background-repeat: no-repeat;
+        background-position: bottom;
+        background-size: 50% 20px;
+        display: inline-block;
+`;
+
+export const NavSection = styled.ul`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 16px;
+    justify-items: center;
+    list-style-type: none;
+    margin: 0;
+    padding: 20px 0 10px;
+`;
+
+export const StyledNavLink = styled(NavLink)`
+    display: inline-block;
+    padding-bottom: 15px;
+    transition: transform 300ms ease, filter 300ms ease;
+
+    &:hover {
+        transform: scale(1.02);
+        filter: brightness(1.3);
+    }
+
+    &.active {
+        filter: brightness(2.5);
+    }
+`;
+
+export const StyledUnderline = styled.img`
+    width: 700px;
+    margin: -340px auto;
+    z-index: -1;
+`;
+
+export const StyledCorner = styled.div`
+    position: absolute;
+    top: 20px;
+    ${({ $position = 'right' }) =>
+        $position === 'left'
+            ? 'left: 20px;'
+            : 'right: 20px; transform: scaleX(-1);'}
+    width: 150px;
+    height: 150px;
+    background: url(${cornerImage}) no-repeat center center;
+    background-size: contain;
+    z-index: 100;
+    filter: brightness(0.3);
+`;
