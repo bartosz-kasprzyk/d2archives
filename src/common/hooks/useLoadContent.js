2409c9f392444804b785b5bb70f423ed7e67f65a
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-09-14 02:46:27 +0200

    deleted polish content

--- src/common/hooks/useLoadContent.js
+++ src/common/hooks/useLoadContent.js
@@ -1,26 +1,15 @@
 import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
 
 const useLoadContent = () => {
-    const location = useLocation();
     const [state, setState] = useState({
         status: 'loading',
         content: null
     });
 
     useEffect(() => {
-        const searchParams = new URLSearchParams(location.search);
-        const lang = searchParams.get('lang') || 'en';
-
         const loadContent = async () => {
             try {
-                let contentModule;
-
-                if (lang === 'pl') {
-                    contentModule = await import('../translations/pl.json');
-                } else {
-                    contentModule = await import('../translations/en.json');
-                }
+                const contentModule = await import('../config/data.json');
 
                 setTimeout(() => {
                     setState({
@@ -30,7 +19,7 @@ const useLoadContent = () => {
                 }, 800);
 
             } catch (error) {
-                console.error('Error loading language file:', error);
+                console.error('Error loading content:', error);
                 setState({
                     status: 'error',
                     content: null
@@ -39,7 +28,7 @@ const useLoadContent = () => {
         };
 
         loadContent();
-    }, [location.search]);
+    }, []);
 
     return state;
 };
