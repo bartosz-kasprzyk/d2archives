ff85b1734dd890ecdcb65360aa3e2d683a5b2c62
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-09-18 17:20:24 +0200

    set the position of loading screen to fixed

--- src/common/Loading/styled.js
+++ src/common/Loading/styled.js
@@ -1,8 +1,16 @@
 import styled from "styled-components";
 
 export const LoadingWrapper = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    padding-top: 300px;
+    z-index: 1000;
     display: flex;
     justify-content: center;
+    align-items: center;
 `;
 
 export const StyledLoading = styled.img`
