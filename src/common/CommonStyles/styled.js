df62cdc3a0d60caf5af461abc02bb0e99f0b3c9c
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-09-22 23:00:34 +0200

    added underline to links in text

--- src/common/CommonStyles/styled.js
+++ src/common/CommonStyles/styled.js
@@ -8,12 +8,10 @@ export const StyledText = styled.p`
 
 export const StyledLink = styled(Link)`
     color: #ca8120;
-    text-decoration: none;
     transition: filter 0.3s ease;
 
     &:hover {
         filter: brightness(1.5);
-        text-decoration: underline;
     }
 `;
 
