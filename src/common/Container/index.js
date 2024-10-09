de16c77c2afb5f32e6514bb756d1b91396e0f433
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-10-09 18:15:44 +0200

    extended content fade-in duration and adjusted keyframe distribution

--- src/common/Container/index.js
+++ src/common/Container/index.js
@@ -3,13 +3,13 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 70%;
   margin: 56px auto;
-  animation: fadeIn 0.2s ease-in-out;
+  animation: fadeIn 500ms ease-in-out;
 
   @keyframes fadeIn {
     0% {
         opacity: 0;
     }
-    50% {
+    25% {
         opacity: 0;
     }
     100% {
