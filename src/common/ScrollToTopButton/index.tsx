5ab5544d8cb07f407861281e5333c1000f669731
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-11-05 13:56:33 +0100

    deleted unnecessary data

--- src/common/ScrollToTopButton/index.tsx
+++ src/common/ScrollToTopButton/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { ButtonWrapper, ButtonBar, StyledButton } from './styled';
 
 export const ScrollToTopButton = () => {
